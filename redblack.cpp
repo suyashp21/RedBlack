8c9e72ccf96c6b5f10173d8ef32ed747f9fe0458
U-BSD\397142 <397142@SHS-10D9C06DD.beaverton.k12.or.us>
2022-04-25 12:21:47 -0700

    Second Commit 4/25

--- redblack.cpp
+++ redblack.cpp
@@ -9,17 +9,32 @@ struct node {
   node* parent;
   node* left;
   node* right;
-}
+};
 
+void add(node* n1, node* n);
 node* search(node* n, int k);
   
 int main() {
   node* root = NULL;
-  char* action[10];
+  char action[10];
   int input;
   while (true) {
     cout << "What would you like to do? (ADD, FILE, SEARCH, DELETE, PRINT, QUIT) "; cin >> action;
     if (strcmp(action, "ADD") == 0 || strcmp(action, "add") == 0) {
+      cout << "Enter the number to add: "; cin >> input;
+      node* n = new node();
+      n->data = input;
+      n->parent = NULL;
+      n->left = NULL;
+      n->right = NULL;
+      n->color = 0;
+      if (root == NULL) {
+	n->color = 1;
+	root = n;
+      }
+      else {
+	add(root, n);
+      }
     }
     else if (strcmp(action, "FILE") == 0 || strcmp(action, "file") == 0) {
     }
@@ -40,10 +55,35 @@ int main() {
     else if (strcmp(action, "QUIT") == 0 || strcmp(action, "quit") == 0) {
       break;
     }
+    cout << endl;
+  }
+}
+
+void add(node* n1, node* n) {
+
+  if (n1->data >= n->data) {
+    if (n1->left == NULL) {
+      n1->left = n;
+      n->parent = n1;
+    }
+    else {
+      add(n1->left, n);
+    }
+  }
+
+  else if (n1->data < n->data) {
+    if (n1->right == NULL) {
+      n1->right = n;
+      n->parent = n1;
+    }
+    else {
+      add(n1->right, n);
+    }
   }
 }
 
 node* search(node* n, int k) {
+  // same as binary search tree
   if (n == NULL) {
     return NULL;
   }
